53eb14f9e609bad9eafd93c938677b13d35f482d
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-03-23 10:07:40 +0000

    Add files via upload

---
+++ encode.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    int i;
+    char mems[20];
+    char *ssss="hello world......\r\n";
+    i=strlen(ssss);
+    printf("\033c\033[44;30m\r\n\r\n");
+    for(n=0;n<i;n++){
+        if(ssss[n]==10 || ssss[n]==13){
+            strcpy(mems," <br> ");
+        }else{
+            sprintf(mems,"&#%03d ",(int)ssss[n]);
+        }
+        printf("%s",mems);
+        
+    }
+
+}
+
